32db7255a7c0747a434d25632d737ac8149547cf
LeLapinTroll <lelapintroll@gmail.com>
2015-07-19 17:03:36 +0300

    Removing epiphany
    working with json export

--- controllers/competition.js
+++ controllers/competition.js
@@ -1,5 +1,6 @@
 LeagueManager.controller('CompetitionCtrl', function($scope, $rootScope, $timeout, Restangular) {
 
+	$scope.journee = 2;
 	$scope.showPreviousDays = function(i){
 		for(i;i>1;i--){
 			$('#day'+1).slideDown();
--- controllers/presentation.js
+++ controllers/presentation.js
@@ -1,3 +1,7 @@
 LeagueManager.controller('PresentationCtrl', function($scope, $rootScope, $timeout, Restangular) {
 
+  $scope.randomSide = $rootScope.randomArticle(['presentation']);
+  console.log()
+  $("#randomPresentation1 .image").css({'background':'#333333 url(resources/img/articles/article_'+$scope.randomSide.article_id+'.jpg) no-repeat','background-size':'cover'})
+
 });
