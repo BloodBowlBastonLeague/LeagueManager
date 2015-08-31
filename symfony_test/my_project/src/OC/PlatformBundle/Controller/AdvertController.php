4f259c71802513738e857e4bb892bef1528d68ff
Gregory Allemand <gallemand@mobibase.com>
2015-08-31 16:21:38 +0200

    maj tuto symfony

--- symfony_test/my_project/src/OC/PlatformBundle/Controller/AdvertController.php
+++ symfony_test/my_project/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -34,10 +34,42 @@ class AdvertController extends Controller
         // Ici, on récupérera la liste des annonces, puis on la passera au template
 
         // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('OCPlatformBundle:Advert:index.html.twig');
+        // return $this->render('OCPlatformBundle:Advert:index.html.twig');
+
+
+        // return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+        //     'listAdverts' => array()
+        // ));
+
+        // Notre liste d'annonce en dur
+        $listAdverts = array(
+          array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => 1,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Mission de webmaster',
+            'id'      => 2,
+            'author'  => 'Hugo',
+            'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+            'date'    => new \Datetime()),
+          array(
+            'title'   => 'Offre de stage webdesigner',
+            'id'      => 3,
+            'author'  => 'Mathieu',
+            'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+            'date'    => new \Datetime())
+        );
+
+        // Et modifiez le 2nd argument pour injecter notre liste
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+          'listAdverts' => $listAdverts
+        ));
     }
 
-    public function viewAction($id, Request $request) {
+    public function viewAction($id /*, Request $request */ ) {
         /** /
         /**/
 
@@ -73,7 +105,19 @@ class AdvertController extends Controller
 
 
 
-        return $this->render('OCPlatformBundle:Advert:view.html.twig',array('id'  => $id));
+        // return $this->render('OCPlatformBundle:Advert:view.html.twig',array('id'  => $id));
+
+        $advert = array(
+          'title'   => 'Recherche développpeur Symfony2',
+          'id'      => $id,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()
+        );
+
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+          'advert' => $advert
+        ));
     }
 
     public function viewSlugAction($year,$slug,$_format) {  
@@ -105,15 +149,37 @@ class AdvertController extends Controller
 
           return $this->redirect($this->generateUrl('oc_platform_view', array('id' => 5)));
         }
+        $advert = array(
+          'title'   => 'Recherche développpeur Symfony2',
+          'id'      => $id,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()
+        );
 
-        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig',array('advert' => $advert));
       }
 
-      public function deleteAction($id) {
+    public function deleteAction($id) {
         // Ici, on récupérera l'annonce correspondant à $id
-
         // Ici, on gérera la suppression de l'annonce en question
-
         return $this->render('OCPlatformBundle:Advert:delete.html.twig');
-      }
+    }
+
+    public function menuAction($limit) {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+          array('id' => 2, 'title' => 'Recherche développeur Symfony2'),
+          array('id' => 5, 'title' => 'Mission de webmaster'),
+          array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+          // Tout l'intérêt est ici : le contrôleur passe
+          // les variables nécessaires au template !
+          'listAdverts' => $listAdverts
+        ));
+
+    }
 }
---
+++ symfony_test/my_project/src/OC/PlatformBundle/Entity/Advert.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Advert
+ *
+ * @ORM\Table(@ORM\Table(name="oc_advert"))
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Entity\AdvertRepository")
+ */
+class Advert
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Advert
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Advert
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Advert
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Advert
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
