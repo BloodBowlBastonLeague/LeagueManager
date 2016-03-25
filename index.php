3a8dd9b74a2507f4c4bccf72b416df2abb7dc6fb
Xavier Olland <xavier.olland@gmail.com>
2016-03-25 13:45:58 +0200

    More fixes on forum style

--- index.php
+++ index.php
@@ -15,6 +15,25 @@
   <link rel="stylesheet" href="css/app.css" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
 
+  <link rel="apple-touch-icon" sizes="57x57" href="/resources/favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/resources/favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/resources/favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/resources/favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/resources/favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/resources/favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/resources/favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/resources/favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/resources/favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/resources/favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/resources/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/resources/favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/resources/favicon/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/resources//ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+
 </head>
 <body>
   <header>
