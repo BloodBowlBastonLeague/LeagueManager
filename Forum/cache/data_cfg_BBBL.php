c5e240330697e998848a03f762e9ec0d7495f61f
Xavier Olland <xavier.olland@gmail.com>
2016-08-20 14:44:06 +0300

    Forum cache

--- Forum/cache/data_cfg_BBBL.php
+++ Forum/cache/data_cfg_BBBL.php
@@ -1,4 +1,4 @@
 <?php exit; ?>
-1495053332
+1503229053
 159
-a:5:{s:4:"name";s:4:"BBBL";s:9:"copyright";s:13:"© BBBL, 2016";s:13:"style_version";s:5:"1.1.0";s:13:"phpbb_version";s:5:"3.1.9";s:8:"filetime";i:1463517220;}
+a:5:{s:4:"name";s:4:"BBBL";s:9:"copyright";s:13:"© BBBL, 2016";s:13:"style_version";s:5:"1.1.0";s:13:"phpbb_version";s:5:"3.1.9";s:8:"filetime";i:1471534780;}
--- Forum/config.php
+++ Forum/config.php
@@ -2,11 +2,11 @@
 // phpBB 3.1.x auto-generated configuration file
 // Do not change anything in this file!
 $dbms = 'phpbb\\db\\driver\\mysqli';
-$dbhost = 'mysql51-37.perso';
-$dbport = '';
-$dbname = 'bbblysgkbbbl';
-$dbuser = 'bbblysgkbbbl';
-$dbpasswd = 'K6KtE6WW';
+$dbhost = 'localhost';
+$dbport = '3306';
+$dbname = 'bbbl2';
+$dbuser = 'root';
+$dbpasswd = 'root';
 $table_prefix = 'forum_';
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
