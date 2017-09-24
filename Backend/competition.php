13e7589a1f251d16f796bddfb36f5eabe1cf2149
Xavier Olland <xavier.olland@gmail.com>
2017-09-24 16:18:55 +0300

    New stats function in competition

--- Backend/competition.php
+++ Backend/competition.php
@@ -8,7 +8,10 @@ include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
 include($phpbb_root_path . 'config.' . $phpEx);
 
+include('statistics.php');
+
 $con = mysqli_connect($dbhost,$dbuser,$dbpasswd,$dbname);
+
 
 if (!$con) { die('Could not connect: ' . mysqli_error()); }
   mysqli_set_charset($con,'utf8');
@@ -17,8 +20,9 @@ if (!$con) { die('Could not connect: ' . mysqli_error()); }
 	$result = mysqli_query($con, $sql);
 	$competition = mysqli_fetch_object($result);
 
-    $var = [];
-    $sql = 'SELECT
+    //Standing
+    $competition->standing = [];
+    $sqlStanding = 'SELECT
           id,
           logo,
           team,
@@ -33,7 +37,8 @@ if (!$con) { die('Could not connect: ' . mysqli_error()); }
           SUM(sustainedcasualties_2 ) - SUM(sustainedcasualties_1) AS S,
           SUM( case when score_1 > score_2 then 3 else 0 end
               + case when score_1 = score_2 AND score_1 IS NOT NULL then 1 else 0 end
-          ) AS Pts
+          ) AS Pts,
+          0 AS confrontation
           FROM (
           SELECT site_matchs.id AS m, site_teams.id AS id, site_teams.logo AS logo, site_teams.name AS team, site_teams.color_1 AS color_1, site_teams.color_2 AS color_2, site_coachs.name AS coach, score_1, score_2, sustainedcasualties_1, sustainedcasualties_2, sustaineddead_1, sustaineddead_2 FROM site_matchs
           LEFT JOIN site_teams ON site_teams.id=site_matchs.team_id_1
@@ -47,79 +52,48 @@ if (!$con) { die('Could not connect: ' . mysqli_error()); }
           ) AS a
           GROUP BY id
           ORDER BY Pts DESC';
-    $result = mysqli_query($con, $sql);
-    while($data = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-      array_push($var, $data);
+
+    $resultStanding = $con->query($sqlStanding);
+    while($dataStanding = mysqli_fetch_array($resultStanding,MYSQLI_ASSOC)) {
+      array_push($competition->standing, $dataStanding);
     }
-    $i = 0;
-    $result = mysqli_query($con, $sql);
-  foreach($var as $var2) {
 
-      if ( $var[$i][Pts] == $var[$i-1][Pts] ) {
-
-        $sqlConfrontation = 'SELECT
-          case when score_1 > score_2 then 2 else case when score_1 = score_2 then 1 else 0 end end
+    //Managing exaequo
+    $limit = count($competition->standing);
+    for($i = 0; $i <= $limit; $i++) {
+        if ( $competition->standing[$i][Pts] == $competition->standing[$i-1][Pts] ) {
+          $sqlConfrontation = 'SELECT
+          case when score_1 > score_2 then 2 else case when score_1 = score_2 AND score_1 IS NOT NULL then 1 else 0 end end
           FROM (
           SELECT site_teams.id, score_1, score_2 FROM site_matchs
           LEFT JOIN site_teams ON site_teams.id=site_matchs.team_id_1
           INNER JOIN site_coachs ON site_coachs.id=site_teams.coach_id
-          WHERE competition_id = '.$id.' AND site_matchs.team_id_1 = '.$var[$i][id].' AND site_matchs.team_id_2 = '.$var[$i-1][id].'
+          WHERE competition_id = '.$id.' AND site_matchs.team_id_1 = '.$competition->standing[$i][id].' AND site_matchs.team_id_2 = '.$competition->standing[$i-1][id].'
           UNION
           SELECT site_teams.id, score_2, score_1 FROM site_matchs
           LEFT JOIN site_teams ON site_teams.id=site_matchs.team_id_2
           INNER JOIN site_coachs ON site_coachs.id=site_teams.coach_id
-          WHERE competition_id='.$id.' AND site_matchs.team_id_2 = '.$var[$i][id].' AND site_matchs.team_id_1 = '.$var[$i-1][id].'
+          WHERE competition_id='.$id.' AND site_matchs.team_id_2 = '.$competition->standing[$i][id].' AND site_matchs.team_id_1 = '.$competition->standing[$i-1][id].'
           ) AS a
           GROUP BY id';
 
-          $row = mysqli_fetch_row(mysqli_query($con, $sqlConfrontation));
-      }
-      else{
-        $row = [1];
-      }
+          $row = mysqli_fetch_row($con->query($sqlConfrontation));
 
-      $var[$i]['confrontation'] = $row[0];
-      $i++;
+        }
+        else{
+            $row = [1];
+        }
+      $competition->standing[$i]['confrontation'] = $row[0];
+
     }
 
-    $currentCompetition = "SELECT id FROM site_matchs WHERE competition_id=".$id;
-    $playersStats = [];
+    //Leaderboard
+    $competition->playersStats = [];
 
-    $scorers = new stdClass();
-    $scorers->type = 'scorers';
-    $scorers->title = 'Meilleurs marqueurs';
-    $players = [];
-    $resultScorers = mysqli_query($con,"SELECT p.id as player, p.name, t.id AS team_id, t.name AS team, t.logo, SUM(s.inflictedtouchdowns) AS TD, SUM(s.inflictedmetersrunning) AS yards, SUM(s.inflictedcatches) AS catches FROM site_players_stats AS s LEFT JOIN site_players AS p ON p.id=s.player_id LEFT JOIN site_teams AS t ON t.id=p.team_id WHERE s.inflictedtouchdowns>0 AND t.active=1 AND s.matchplayed=1 AND s.match_id IN (".$currentCompetition.") GROUP BY p.id ORDER BY SUM(s.inflictedtouchdowns) DESC");
-    while($dataScorers = mysqli_fetch_array($resultScorers,MYSQLI_ASSOC)) {
-      array_push($players, $dataScorers);
+    $stats = ['scorer','thrower','tackler','killer','intercepter','catcher','punchingball'];
+    foreach($stats as $stat){
+        array_push($competition->playersStats, leaders($con,[$stat,$id]));
     }
-    $scorers->players = $players;
-    array_push($playersStats, $scorers);
-
-    $tacklers = new stdClass();
-    $tacklers->type = 'tacklers';
-    $tacklers->title = 'Meilleurs bastonneurs';
-    $players = [];
-    $resultTacklers = mysqli_query($con,"SELECT p.id as player, p.name, t.id AS team_id, t.name AS team, t.logo, SUM(s.inflictedcasualties) + SUM(s.inflicteddead) AS injuries, SUM(s.inflictedcasualties) AS casualties, SUM(s.inflicteddead) AS dead FROM site_players_stats AS s LEFT JOIN site_players AS p ON p.id=s.player_id LEFT JOIN site_teams AS t ON t.id=p.team_id WHERE (s.inflictedcasualties>0 OR s.inflicteddead>0) AND t.active=1 AND s.matchplayed=1 AND s.match_id IN (".$currentCompetition.") GROUP BY p.id ORDER BY SUM(s.inflictedcasualties) + SUM(s.inflicteddead) DESC");
-    while($dataTacklers = mysqli_fetch_array($resultTacklers,MYSQLI_ASSOC)) {
-      array_push($players, $dataTacklers);
-    }
-    $tacklers->players = $players;
-    array_push($playersStats, $tacklers);
-
-    $throwers = new stdClass();
-    $throwers->type = 'throwers';
-    $throwers->title = 'Meilleurs passeurs';
-    $players = [];
-    $resultThrowers = mysqli_query($con,"SELECT p.id as player, p.name, t.id AS team_id, t.name AS team, t.logo, SUM(s.inflictedpasses) AS passes, SUM(s.inflictedmeterspassing) AS yards FROM site_players_stats AS s LEFT JOIN site_players AS p ON p.id=s.player_id LEFT JOIN site_teams AS t ON t.id=p.team_id WHERE s.inflictedpasses>0 AND t.active=1 AND s.matchplayed=1 AND s.match_id IN (".$currentCompetition.") GROUP BY p.id ORDER BY SUM(s.inflictedmeterspassing) DESC");
-    while($dataThrowers = mysqli_fetch_array($resultThrowers,MYSQLI_ASSOC)) {
-      array_push($players, $dataThrowers);
-    }
-    $throwers->players = $players;
-    array_push($playersStats, $throwers);
-    $competition->playersStats = $playersStats;
-
-    $competition->standing = $var;
 
     echo json_encode($competition,JSON_NUMERIC_CHECK);
   die();
---
+++ Backend/statistics.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Get the leaders in an area of the game
+ * @param $con la connexion à la BDD
+ * @param $params les données à insérer
+ */
+function leaders($con, $params){
+
+    $leaders = new stdClass();
+    $leaders->type = $params[0];
+    $leaders->players = [];
+
+    switch ($params[0]){
+        case "scorer":
+            $leaders->stats = ['inflictedtouchdowns','inflictedmetersrunning','inflictedcatches'];
+            break;
+        case "thrower":
+            $leaders->stats = ['inflictedmeterspassing','inflictedpasses'];
+            break;
+        case "tackler":
+            $leaders->stats = ['inflictedinjuries','inflictedcasualties','inflicteddead'];
+            break;
+        case "killer":
+            $leaders->stats = ['inflicteddead'];
+            break;
+        case "intercepter":
+            $leaders->stats = ['inflictedinterceptions'];
+            break;
+        case "punchingball":
+            $leaders->stats = ['sustainedinjuries'];
+            break;
+        case "catcher":
+            $leaders->stats = ['inflictedcatches'];
+            break;
+    }
+
+    //Prepare fields and Order by for the query
+    $fields = "";
+    $orderBy = " ORDER BY";
+    foreach( $leaders->stats as $key=>$stat ){
+        $fields = $fields.', SUM(s.'.$stat.') AS stat'.$key;
+        if($orderBy == " ORDER BY" ){
+            $orderBy = $orderBy.' SUM(s.'.$stat.') DESC';
+        }
+        else{
+            $orderBy = $orderBy.', SUM(s.'.$stat.') DESC';
+        }
+    }
+    //Manage competition filter
+    if($params[1]){
+        $where = " AND s.match_id IN (SELECT id FROM site_matchs WHERE competition_id=".$params[1].")";
+    }
+    else{
+       $where = "";
+    }
+
+    $sqlPlayers = "SELECT p.id as player, p.name, t.id AS team_id, t.name AS team, t.logo".$fields."
+        FROM site_players_stats AS s
+        LEFT JOIN site_players AS p ON p.id=s.player_id
+        LEFT JOIN site_teams AS t ON t.id=p.team_id
+        WHERE s.".$leaders->stats[0].">0".$where."
+        GROUP BY p.id".$orderBy;
+
+    $resultPlayers = $con->query($sqlPlayers);
+    while($dataPlayers = mysqli_fetch_array($resultPlayers,MYSQLI_ASSOC)) {
+        array_push($leaders->players, $dataPlayers);
+    }
+
+    return $leaders;
+
+}
+
+?>
