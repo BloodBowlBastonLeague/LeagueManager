cddfa266eb9da5de3a9fc771ee9c129be68c8157
Xavier Olland <xavier.olland@gmail.com>
2018-03-12 19:15:25 +0200

    calendar start

--- Backend/competition.php
+++ Backend/competition.php
@@ -103,6 +103,55 @@ function competition_standing($con, $id){
 
 };
 
+//Get fixtures
+function competition_calendar($con, $id){
+    mysqli_set_charset($con,'utf8');
+    $calendar = [];
+    $sqlRound = "SELECT DISTINCT(round) FROM site_matchs WHERE competition_id=".$id." ORDER BY round";
+    $resultRound = $con->query($sqlRound);
+    while($rounds = $resultRound->fetch_assoc()){
+        $matches = [];
+        $sql2 = "SELECT site_matchs.id, site_matchs.cyanide_id, site_matchs.contest_id, DATE_FORMAT(site_matchs.started,'%Y%m%dT%H%i%sZ') AS started, round,
+        t1.coach_id as coach_id_1, team_id_1, t1.name as name_1, t1.logo as logo_1, score_1,
+        t2.coach_id as coach_id_2, team_id_2, t2.name as name_2, t2.logo as logo_2, score_2
+        FROM site_matchs
+        LEFT JOIN site_teams as t1 ON t1.id=site_matchs.team_id_1
+        LEFT JOIN site_teams as t2 ON t2.id=site_matchs.team_id_2
+        WHERE competition_id=".$id." AND round=".$data[round];
+        $result2 = $con->query($sql2);
+
+        while($data2 = $result2->fetch_assoc()) {
+            //Ajout des pronos
+            $prono=[];
+            $data2[bets]=[];
+            $sqlProno="SELECT match_id, coach_id, team_score_1, team_score_2, stake FROM site_bets
+              WHERE match_id=".$data2[id];
+            $resultProno = $con->query($sqlProno);
+            while($dataProno = $resultProno->fetch_assoc()) {
+                $data2[bets][]=$dataProno;
+            }
+            array_push($matches, $data2);
+        }
+        $data[matchs] = $var2;
+        $var3 = [];
+        $sql3 = "SELECT CASE WHEN c.competition_mode='Coupe' THEN MAX(m.round) ELSE MIN(m.round) END
+          FROM site_matchs AS m
+          INNER JOIN site_competitions AS c ON c.id = m.competition_id
+          WHERE m.competition_id=".$id." AND m.started IS NULL";
+          $result3 = $con->query($sql3);
+
+          while($data3 = $result2->fetch_assoc()) {
+            $data[currentDay] = $data3[0];
+        }
+
+        array_push($calendar, $data);
+    }
+
+
+    echo json_encode($var,JSON_NUMERIC_CHECK);
+    die();
+};
+
 //Get competition statistics
 function competition_stats($con,$competition){
     //Leaderboard
@@ -121,6 +170,7 @@ function competition_update($con,$params){
         competition_update_matches($con,$params);
     }
     elseif ($params[5] == 'swiss') {
+      echo "test";
         competition_next_day($con,$params);
     }
 };
